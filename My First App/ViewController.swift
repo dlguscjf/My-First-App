dd3fa34e20bb757e4083671f6e43bdc5992a6f86
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-12 15:12:25 +0900

    but, text, textbook, uicolor

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var textbox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.blue
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    @IBAction func but(_ sender: UIButton) {
+        text.text = "Hello, " + textbox.text!
+        textbox.text = ""
+    }
 }
-
